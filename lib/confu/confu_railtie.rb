ca10aa079bfef71b958212479d293097e64016ed
Krasimir Angelov <krasio@codingspree.net>
2010-10-08 13:19:30 +0300

    Fixed call to Rails.root

--- lib/confu/confu_railtie.rb
+++ lib/confu/confu_railtie.rb
@@ -2,7 +2,7 @@ module Confu
   module Rails
     class Railtie < ::Rails::Railtie
       config.before_configuration do
-        Confu.load_config_files(File.join(Rails.root, 'config', 'site'))
+        Confu.load_config_files(File.join(::Rails.root, 'config', 'site'))
       end
     end
   end
